83652acedd672abbf9be5375955636ea39fbf645
Rohith <Rohith@Rohith>
2024-06-25 19:16:18 +0530

    first commit

---
+++ CoreJava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+
+	int serialNum=07;//data variables
+	String name;
+	int age;
+	void show()//member function
+	{
+		System.out.println("Name: "+name+"\t age"+age+"\t serialNumber= "+serialNum);
+	}
+}
---
+++ CoreJava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Encapsulation ob=new Encapsulation();
+		ob.name="rohit";
+		ob.age=21;
+		ob.serialNum=88;
+		ob.show();
+	}
+
+}
